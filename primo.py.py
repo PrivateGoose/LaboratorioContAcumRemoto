52b49e0e49beaf38b4c10fa6455c8601fe389eca
Perez Quintero <000402095@bga.upb>
2019-03-08 11:32:42 -0500

    Ya casi calculo un primo

--- primo.py.py
+++ primo.py.py
@@ -1,6 +1,5 @@
 a=int(input("Digite el primer número: "))
-b=int(input("Digite el segundo número: "))
 
-if a%b==0:
-  
-  print("{} es divisible exacto entre {}".format(a,b))
+for i in range (1,a):
+
+  print ("El residuo entre ", a, " y ", i, " es: ", a%i)
